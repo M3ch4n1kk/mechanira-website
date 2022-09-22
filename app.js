7192536cbe6322647b82966a5557e7af28b5783a
mecha does code <69651144+M3ch4n1kk@users.noreply.github.com>
2022-09-22 10:36:44 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -40,3 +40,16 @@ if(cookie_consent = ""){
 }else{
     document.getElementById("cookieNotice").style.display = "block";
 }
+
+
+const menuBtn = document.querySelector('.menu-btn')
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open')
+        menuOpen = false;
+    }
+})
