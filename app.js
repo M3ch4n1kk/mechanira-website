83896c1685dfb0c892aaf0487de2f50b0b890a3f
mecha does code <69651144+M3ch4n1kk@users.noreply.github.com>
2022-10-21 13:13:19 +0200

    menu toggle function

--- app.js
+++ app.js
@@ -41,19 +41,6 @@ if(cookie_consent = ""){
     document.getElementById("cookieNotice").style.display = "block";
 }
 
-/*
-const menuBtn = document.querySelector('.menu-btn')
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open')
-        menuOpen = false;
-    }
-})
-*/
 
 var menu = document.getElementById("menu");
 
